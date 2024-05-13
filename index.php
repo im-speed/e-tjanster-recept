d194bf6702da38911aaa869f5d5cf9d4cf6c5957
Lukas Wiman <lukas.wiman.3168@student.uu.se>
2024-05-13 13:36:43 +0200

    css Background

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/button.css">
+    <link rel="stylesheet" href="CSS/headerMain.css">
     <title>Document</title>
 </head>
 <header><h1>Oliver noodles</h1></header>
