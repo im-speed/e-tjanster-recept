1d3313ef5e31985de04764c01b2b9e2e32420fb8
Lukas Wiman <lukas.wiman.3168@student.uu.se>
2024-05-13 13:33:13 +0200

    Button Ex

--- index.php
+++ index.php
@@ -3,10 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/button.css">
     <title>Document</title>
 </head>
 <header><h1>Oliver noodles</h1></header>
 <body>
-    
+    <div class="button">
+
+    <button>Meny</button>
+
+    </div>
+    <div>
+
+    </div>
 </body>
 </html>
