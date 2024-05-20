12a2fde1398d700f963ef49345dcf8799bf5bf35
MrNinjaPowha <you@example.com>
2024-05-20 17:24:02 +0200

    Lade till typer till funktionerna

--- db-queries.php
+++ db-queries.php
@@ -1,27 +1,27 @@
 <?php
-function matchEmail($conn, $newEmail){
+function matchEmail(SQLite3 $conn, string $newEmail)
+{
     $searchExistingEmail = $conn->prepare("SELECT * FROM user WHERE Email=:email;");
     $searchExistingEmail->bindParam(':email', $newEmail, SQLITE3_TEXT);
-    $matchingEmail = $searchExistingEmail->execute();   
+    $matchingEmail = $searchExistingEmail->execute();
 
-    if($matchingEmail){
-        if(!$matchingEmail->fetchArray(SQLITE3_ASSOC)){
-          $conn->close();
-          return false;
-        }    
-        else {
+    if ($matchingEmail) {
+        if (!$matchingEmail->fetchArray(SQLITE3_ASSOC)) {
+            $conn->close();
+            return false;
+        } else {
             $conn->close();
             return true;
         }
-    }    
-    else {
+    } else {
         $conn->close();
         return "SQL-Query failed";
     }
 }
 
-function addNewUser($conn, $email, $hashedPassword){
-    $stmt = $conn->prepare($sql = "INSERT INTO 'user' ('email', 'password') VALUES (:email, :password)");
+function addNewUser(SQLite3 $conn, string $email, string $hashedPassword)
+{
+    $stmt = $conn->prepare("INSERT INTO 'user' ('email', 'password') VALUES (:email, :password)");
     $stmt->bindParam(':email', $email, SQLITE3_TEXT);
     $stmt->bindParam(':password', $hashedPassword, SQLITE3_TEXT);
 
