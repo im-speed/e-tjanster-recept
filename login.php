846630c01acf57d409090e05971c46aaf7805eea
Humpy123 <oliver@wahrolen.se>
2024-05-20 13:56:38 +0200

    börjat på login css

--- login.php
+++ login.php
@@ -4,21 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="CSS/index.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sixtyfour&family=Tilt+Prism&family=Tilt+Warp&family=Ysabeau+SC:wght@1..1000&display=swap" rel="stylesheet">
 </head>
-<body>
+<body id="login_body">
+    <div id="login_container">
+        <div id="login_form_container" >
         <h2>Logga in</h2>
-        <form id="logInForm" name="logInForm" action="handleLogin.php" method="post">
-            <label for="email">Email</label>
-            <input type="text" id="email" name="email"required pattern="[^@\s]+@[^@\s]+\.[^@\s]+" title="Emails require an @, followed by atleast 2 characters with a . in between."><br>
-            <label for="password">Lösenord</label>
-            <input id="password" name="password" required><br><br>
-            <input id="submit" type="submit" value="Logga in">
-        </form>
-        <a href='register.php' id='submit' class='flexcenter btn newspapertext zoom justpaper'>Har du inget konto? Registrera dig här!</a>
+            <form id="login_Form" name="logInForm" action="handleLogin.php" method="post">
+                <label for="email">Email</label><br>
+                <input type="text" id="email" name="email"required pattern="[^@\s]+@[^@\s]+\.[^@\s]+" title="Emails require an @, followed by atleast 2 characters with a . in between."><br>
+                <label for="password">Lösenord</label><br>
+                <input type="password" id="password" name="password" required><br><br>
+                <input id="submit" type="submit" class="button" value="Logga in">
+            </form>
+
+        </div>
+        <a href='register.php' id='submit' class='button'>Har du inget konto? Registrera dig här!</a>
+    </div>
+       
 </body>
 </html>
  
