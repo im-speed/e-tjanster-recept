6bf8b85a6904bfb1a18e5c5a341d6e4b4e62fb70
MrNinjaPowha <you@example.com>
2024-05-27 14:02:59 +0200

    Added functional categories on start

--- include/db/functions/recipe.php
+++ include/db/functions/recipe.php
@@ -1,9 +1,30 @@
 <?php
 
+require_once __DIR__ . "/../models/recipe.php";
+
 function mark_deleted_recipe(SQLite3 $conn, int $recipe_id): bool
 {
     $stmt = $conn->prepare("UPDATE recipe SET Deleted = 1 WHERE RecipeID = :id");
     $stmt->bindParam(":id", $recipe_id);
 
     return $stmt->execute() == true;
+}
+
+function list_all_recipes(SQLite3 $conn): ?array
+{
+    $stmt = $conn->prepare("SELECT * FROM recipe");
+
+    if (!$res = $stmt->execute()) {
+        return null;
+    }
+
+    $recipes = [];
+    while ($row = $res->fetchArray()) {
+        $recipe = Recipe::from_row($row);
+        if (!$recipe->isDeleted()) {
+            array_push($recipes, $recipe);
+        }
+    }
+
+    return $recipes;
 }
